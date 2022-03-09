2ceb8d64215598b3a4752e2fe81d10cbca80b725
wa_ab <waqas@itinsync.com>
2022-03-09 14:36:57 +0500

    Get Requests made

---
+++ src/data/posts.ts
@@ -0,0 +1,28 @@
+const posts = [
+    {"id": 1, "post_id": 1, "description": "Malignant neoplasm of abdominal esophagus"},
+    {"id": 2, "post_id": 2, "description": "Hip joint replacement"},
+    {
+        "id": 3,
+        "post_id": 3,
+        "description": "Cerebellar or brain stem contusion with open intracranial wound, with concussion, unspecified"
+    },
+    {
+        "id": 4,
+        "post_id": 4,
+        "description": "Noncollision motor vehicle traffic accident while boarding or alighting injuring motorcyclist"
+    },
+    {"id": 5, "post_id": 5, "description": "Other signs and symptoms involving emotional state"},
+    {"id": 6, "post_id": 6, "description": "Apparent life threatening event in infant"},
+    {
+        "id": 7,
+        "post_id": 7,
+        "description": "Open fracture of base of skull with subarachnoid, subdural, and extradural hemorrhage, with no loss of consciousness"
+    },
+    {"id": 8, "post_id": 8, "description": "Hypertelorism of orbit"},
+    {"id": 9, "post_id": 9, "description": "Molluscum contagiosum"},
+    {
+        "id": 10,
+        "post_id": 10,
+        "description": "Hemorrhage from placenta previa, unspecified as to episode of care or not applicable"
+    }]
+module.exports = posts;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 const express = require('express');
+const posts_made = require('./data/posts');
+const messages_made = require('./data/posts');
 const app = express();
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Started')
+})
+app.get('/posts', (req: any, res: any) => {
+    res.json(posts_made)
+})
+app.get('/messages', (req: any, res: any) => {
+    res.json(messages_made)
 })

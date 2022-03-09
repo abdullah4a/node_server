30465022d1bfbbd0ff5b3727abb627b91b3218f6
wa_ab <waqas@itinsync.com>
2022-03-09 14:48:46 +0500

    Starting point

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ const app = express();
 app.listen(3000, () => {
     console.log('Started')
 })
+app.get('/', (req: any, res: any) => {
+    res.send('<h1>API is Working</h1>')
+})
 app.get('/posts', (req: any, res: any) => {
     res.json(posts_made)
 })
